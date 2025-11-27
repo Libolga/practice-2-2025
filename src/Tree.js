bb24c8b6577eb64b0018e175cee625bebc3081bc
Libolga <108710219+Libolga@users.noreply.github.com>
2025-11-27 23:22:19 +1000

    Create Tree.js

---
+++ src/Tree.js
@@ -0,0 +1,57 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  addNode(data) {
+    const newNode = new Node(data);
+    
+    if (this.root === null) {
+      this.root = newNode;
+      return;
+    }
+
+    let current = this.root;
+    while (true) {
+      if (data < current.data) {
+        if (current.left === null) {
+          current.left = newNode;
+          return;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  hasNode(data) {
+    let current = this.root;
+    
+    while (current !== null) {
+      if (data === current.data) {
+        return true;
+      } else if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    
+    return false;
+  }
+}
+
+module.exports = { Tree, Node };
